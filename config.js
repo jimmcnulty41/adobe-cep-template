56145167645fb8f107693c2ebedcaca7891fe488
James McNulty <jimmcnulty41@gmail.com>
2018-07-16 03:16:25 -0700

    Switch config.ts for config.js
    Previously, changes to the config file would require a rebuild. Since it
    is now in javascript, this should not be necessary

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    config :{
+        bundleId: "company.mybundle",
+        extensionId: "company.mybundle.myextension",
+        menuTitle: "My Extension!",
+        dirName: "builtExtension",
+        appId: 6
+        // 0     "Photoshop"
+        // 1     "InDesign"
+        // 2     "InCopy"
+        // 3     "Illustrator"
+        // 4     "Premiere Pro"
+        // 5     "Prelude"
+        // 6     "After Effects",
+        // 7     "Animate (formerly Flash Pro)",
+        // 8     "Audition",
+        // 9     "Dreamweaver",
+        // 10    "Muse",
+        // 11    "Bridge",
+    }
+}
