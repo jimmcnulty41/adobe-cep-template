d8e9331a3021057438525e1188d0cc1f26f15edf
James McNulty <jimmcnulty41@gmail.com>
2018-07-16 03:11:19 -0700

    Provide comment in config

--- config.ts
+++ config.ts
@@ -1,7 +1,19 @@
 export const config = {
     bundleId: "company.mybundle",
     extensionId: "company.mybundle.myextension",
-    appId: 1,
     menuTitle: "My Extension!",
-    dirName: "builtExtension"
+    dirName: "builtExtension",
+    appId: 6
+    // 0     "Photoshop"
+    // 1     "InDesign"
+    // 2     "InCopy"
+    // 3     "Illustrator"
+    // 4     "Premiere Pro"
+    // 5     "Prelude"
+    // 6     "After Effects",
+    // 7     "Animate (formerly Flash Pro)",
+    // 8     "Audition",
+    // 9     "Dreamweaver",
+    // 10    "Muse",
+    // 11    "Bridge",
 };
